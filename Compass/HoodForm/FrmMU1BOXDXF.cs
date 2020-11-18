a50ed2963390cd2e45603b3dc179ae2dcb875434
felixzhu1989 <zhu-hongfeng@hotmail.com>
2020-11-18 17:03:17 +0800

    增加MU1VVIBOX

---
+++ Compass/HoodForm/FrmMU1BOXDXF.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Common;
+using DAL;
+using Models;
+
+namespace Compass
+{
+    public partial class FrmMU1BOXDXF :MetroFramework.Forms.MetroForm
+    {
+        CategoryService objCategoryService = new CategoryService();
+        MU1BOXDXFService objMU1BOXDXFService = new MU1BOXDXFService();
+        private MU1BOXDXF objMU1BOXDXF = null;
+        public FrmMU1BOXDXF()
+        {
+            InitializeComponent();
+        }
+        public FrmMU1BOXDXF(Drawing drawing, ModuleTree tree) : this()
+        {
+            objMU1BOXDXF = (MU1BOXDXF)objMU1BOXDXFService.GetModelByModuleTreeId(tree.ModuleTreeId.ToString());
+            if (objMU1BOXDXF == null) return;
+            this.Text = drawing.ODPNo + " / Item: " + drawing.Item + " / Module: " + tree.Module + " - " + tree.CategoryName;
+            Category objCategory = objCategoryService.GetCategoryByCategoryId(tree.CategoryId.ToString());
+            pbModelImage.Image = objCategory.ModelImage.Length == 0
+                ? Image.FromFile("NoPic.png")
+                : (Image)new SerializeObjectToString().DeserializeObject(objCategory.ModelImage);
+            FillData();
+        }
+        /// <summary>
+        /// 填数据
+        /// </summary>
+        private void FillData()
+        {
+            if (objMU1BOXDXF == null) return;
+            pbModelImage.Tag = objMU1BOXDXF.MU1BOXDXFId;
+
+            //默认txtQuantity为1
+            txtQuantity.Text = objMU1BOXDXF.Quantity == 0 ? "1" : objMU1BOXDXF.Quantity.ToString();
+        }
+        /// <summary>
+        /// 修改参数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEditData_Click(object sender, EventArgs e)
+        {
+
+            #region 数据验证
+            //必填项目
+            if (pbModelImage.Tag.ToString().Length == 0) return;
+            if (!DataValidate.IsInteger(txtQuantity.Text.Trim()))
+            {
+                MessageBox.Show("请认真检查数量是否填错", "提示信息");
+                txtQuantity.Focus();
+                txtQuantity.SelectAll();
+                return;
+            }
+
+
+            #endregion
+            //封装对象
+            MU1BOXDXF objMU1BOXDXF = new MU1BOXDXF()
+            {
+                MU1BOXDXFId = Convert.ToInt32(pbModelImage.Tag),
+                Quantity = Convert.ToInt32(txtQuantity.Text)
+
+            };
+            //提交修改
+            try
+            {
+                if (objMU1BOXDXFService.EditModel(objMU1BOXDXF) == 1)
+                {
+                    MessageBox.Show("制图数据修改成功", "提示信息");
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ DAL/HoodService/MU1BOXDXFService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DAL
+{
+   public class MU1BOXDXFService : IModelService
+    {
+        public int EditModel(IModel model)
+        {
+            MU1BOXDXF objModel = (MU1BOXDXF)model;
+            //编写带参数的SQL语句
+            StringBuilder sqlBuilder = new StringBuilder();
+            sqlBuilder.Append("Update MU1BOXDXF set Quantity=@Quantity where MU1BOXDXFId=@MU1BOXDXFId");
+            //定义参数数组
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter("@Quantity",objModel.Quantity),
+                new SqlParameter("@MU1BOXDXFId",objModel.MU1BOXDXFId)
+            };
+            try
+            {
+                return SQLHelper.Update(sqlBuilder.ToString(), param);
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("数据库操作出现异常：" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public DataSet GetModelByDataSet(string projectId)
+        {
+            string sql = "select MU1BOXDXFId,MU1BOXDXF.ModuleTreeId,Item,Module,Quantity from MU1BOXDXF";
+            sql += " inner join ModuleTree on MU1BOXDXF.ModuleTreeId=ModuleTree.ModuleTreeId";
+            sql += " inner join DrawingPlan on ModuleTree.DrawingPlanId=DrawingPlan.DrawingPlanId";
+            sql += string.Format(" where ProjectId={0}", projectId);
+            sql += " order by Item,Module";
+            return SQLHelper.GetDataSet(sql);
+        }
+
+        public IModel GetModelById(string id)
+        {
+            return GetModelByWhereSql(string.Format(" where MU1BOXDXFId={0}", id));
+        }
+
+        public IModel GetModelByModuleTreeId(string moduleTreeId)
+        {
+            return GetModelByWhereSql(string.Format(" where ModuleTreeId={0}", moduleTreeId));
+        }
+
+        public IModel GetModelByWhereSql(string whereSql)
+        {
+            string sql =
+                "select MU1BOXDXFId,ModuleTreeId,Quantity from MU1BOXDXF";
+            sql += whereSql;
+            SqlDataReader objReader = SQLHelper.GetReader(sql);
+            MU1BOXDXF objModel = null;
+            if (objReader.Read())
+            {
+                objModel = new MU1BOXDXF()
+                {
+                    MU1BOXDXFId = Convert.ToInt32(objReader["MU1BOXDXFId"]),
+                    ModuleTreeId = Convert.ToInt32(objReader["ModuleTreeId"]),
+                    //最好不要用=null去判断，提示类型转换错误
+                    Quantity = objReader["Quantity"].ToString().Length == 0 ? 0 : Convert.ToInt32(objReader["Quantity"]),
+                };
+            }
+            objReader.Close();
+            return objModel;
+        }
+    }
+}
