d70b19eaf6a3420bf781442960b92dfd4a046a27
felixzhu1989 <zhu-hongfeng@hotmail.com>
2021-10-11 16:49:07 +0800

    测试VS git

--- Compass/Program.cs
+++ Compass/Program.cs
@@ -18,14 +18,8 @@ namespace Compass
             FrmUserLogin objFrmUserLogin=new FrmUserLogin();
             DialogResult result = objFrmUserLogin.ShowDialog();
             //判断登陆是否成功
-            if (result == DialogResult.OK)
-            {
-                Application.Run(new FrmMain());
-            }
-            else
-            {
-                Application.Exit();
-            }
+            if (result == DialogResult.OK) Application.Run(new FrmMain());
+            else Application.Exit();
         }
         //定义一个全局变量，用来保存登陆对象
         public static User ObjCurrentUser = null;
